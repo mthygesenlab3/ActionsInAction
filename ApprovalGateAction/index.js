831f2d8b36595a0089126c332d132c847a7ee4a2
Michael Thygesen <michael.thygesen@lab3.com.au>
2025-02-28 15:38:57 +1100

    updates

--- ApprovalGateAction/index.js
+++ ApprovalGateAction/index.js
@@ -3,12 +3,12 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const tfAction = core.getInput('action', { required: true });
+  const action = core.getInput('action', { required: true });
   const environment = core.getInput('environment', { required: true });
   const currentBranch = core.getInput('current-branch', { required: true });
   const mainBranch = core.getInput('main-branch', { required: true });
 
-  if (environment == 'Prod' && tfAction == 'Apply' && currentBranch != mainBranch){
+  if (environment == 'Prod' && action == 'Apply' && currentBranch != mainBranch){
     core.setOutput("outcome", false);
     return
   }
