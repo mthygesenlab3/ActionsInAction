34784a6a39d94ac8729b8e4aea4a95bb39d21cc9
Michael Thygesen <michael.thygesen@lab3.com.au>
2025-02-28 16:15:13 +1100

    test secret

--- ApprovalGateAction/index.js
+++ ApprovalGateAction/index.js
@@ -1,21 +1,19 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-
-core.setSecret('action');
-
 try {
   // `who-to-greet` input defined in action metadata file
   const action = core.getInput('action', { required: true });
   const environment = core.getInput('environment', { required: true });
   const currentBranch = core.getInput('current-branch', { required: true });
   const mainBranch = core.getInput('main-branch', { required: true });
+  core.setSecret(mainBranch);
 
   if (environment == 'Prod' && action == 'Apply' && currentBranch != mainBranch){
     core.setOutput("outcome", "False");
     core.setFailed(`Cannot run apply from a branch that is not master in Prod environment!`);
     return
-  }
+  } 
 
   core.setOutput("outcome", "True")
   
